424df6760fe2b5ff3555d1387de064299514e812
Poseidon <wangliyingwad@hotmail.com>
2017-02-14 18:02:18 +0800

    2017/2/14 Leo Realm Module import

--- library/src/main/java/com/leo/library/common/ApplicationInitializer.java
+++ library/src/main/java/com/leo/library/common/ApplicationInitializer.java
@@ -9,25 +9,35 @@ import com.leo.bulldoglog.BLog;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * Created by Leo on 2017/2/12.
  */
 
 public class ApplicationInitializer {
-    protected ExecutorService cachedThreadScheduler= Executors.newCachedThreadPool();
+    private ExecutorService cachedThreadScheduler = Executors.newCachedThreadPool();
+    private static final String DB_NAME = "realm.realm";
 
     /**
      * 并行初始化
+     *
      * @param application
      */
-    public void init(final Application application){
+    public void init(final Application application) {
         /* 因为接下来立即使用了该包需同步初始化 */
         BLog.init();
         /* 其他初始化任务无时序要求可多线程处理 */
         cachedThreadScheduler.execute(new Runnable() {
             @Override
             public void run() {
-
+                Realm.init(application);
+                RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                        .name(DB_NAME)
+                        .deleteRealmIfMigrationNeeded()
+                        .build();
+                Realm.setDefaultConfiguration(realmConfiguration);
             }
         });
         cachedThreadScheduler.execute(new Runnable() {
