492cc364c1cb2833103639abe0b7190cfc1081db
Poseidon <wangliyingwad@hotmail.com>
2017-02-20 18:09:23 +0800

    2017/2/20 Leo RxBus import BaseActivity

--- library/src/main/java/com/leo/library/base/BaseActivity.java
+++ library/src/main/java/com/leo/library/base/BaseActivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.leo.library.message.RxBus;
 import com.leo.potato.Potato;
 import com.leo.potato.PotatoInjection;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import io.realm.Realm;
+import rx.Observable;
+import rx.Subscription;
+import rx.functions.Action1;
 
 /**
  * Created by leo on 2017/2/17.
@@ -20,6 +26,8 @@ import io.realm.Realm;
 public abstract class BaseActivity extends Activity {
 
     protected Realm realm;
+
+    private List<WeakReference<Subscription>> subscriptions = new ArrayList<>();
 
 //    private Map<String, WeakReference<ABaseFragment>> fragmentRefs;
 
@@ -42,6 +50,46 @@ public abstract class BaseActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        releaseRxBus();
         realm.close();
+    }
+
+
+    /**
+     * 发送消息总线
+     *
+     * @param tag 消息标记
+     * @param obj
+     */
+    public void rxBusPostMsg(String tag, Object obj) {
+        RxBus.getInstance().rxBusPost(tag, obj);
+    }
+
+    /**
+     * 接受消息回掉
+     *
+     * @param tag
+     * @return
+     */
+    public Action1 rxBusReceiveMsg(String tag) {
+        Action1<Object> action1 = new Action1<Object>() {
+            @Override
+            public void call(Object o) {
+
+            }
+        };
+        WeakReference<Subscription> weakSubscription = new WeakReference<>(RxBus.getInstance()
+                .rxBusReceiveMsg(tag)
+                .subscribe(action1));
+        subscriptions.add(weakSubscription);
+        return action1;
+    }
+
+    private void releaseRxBus() {
+        if (subscriptions.size() > 0) {
+            for (WeakReference<Subscription> subscription : subscriptions) {
+                subscription.get().unsubscribe();
+            }
+        }
     }
 }
--- library/src/main/java/com/leo/library/message/RxBus.java
+++ library/src/main/java/com/leo/library/message/RxBus.java
@@ -1,8 +1,94 @@
 package com.leo.library.message;
 
+
+import java.security.PublicKey;
+import java.util.Vector;
+
+import rx.Observable;
+import rx.functions.Func1;
+import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
+
 /**
  * Created by leo on 2017/2/17.
  */
 
 public class RxBus {
+    private static volatile RxBus instance;
+
+    private Subject<Object, Object> bus;
+
+    private RxBus() {
+        bus = new SerializedSubject<>(PublishSubject.create());
+    }
+
+    public static RxBus getInstance() {
+        if (instance == null) {
+            synchronized (RxBus.class) {
+                instance = new RxBus();
+            }
+        }
+        return instance;
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param tag     消息标记
+     * @param content 消息体
+     */
+    public void rxBusPost(String tag, Object content) {
+        bus.onNext(new EventWrapper(tag, content));
+    }
+
+    /**
+     * 接受消息回掉
+     *
+     * @param tag 筛选标记
+     * @return
+     */
+    public Observable<Object> rxBusReceiveMsg(final String tag) {
+        return bus.filter(new Func1<Object, Boolean>() {
+            @Override
+            public Boolean call(Object o) {
+                if (o instanceof EventWrapper) {
+                    if (((EventWrapper) o).tag.equals(tag))
+                        return true;
+                }
+
+                return false;
+            }
+        }).flatMap(new Func1<Object, Observable<?>>() {
+            @Override
+            public Observable<?> call(Object o) {
+                if (o instanceof EventWrapper) {
+                    return Observable.just(((EventWrapper) o).content);
+                }
+                return null;
+            }
+        });
+    }
+
+    /**
+     * 加tag便于筛选消息
+     */
+    private static class EventWrapper {
+        private String tag;
+        private Object content;
+
+        EventWrapper(String _tag, Object _content) {
+            this.tag = _tag;
+            this.content = _content;
+        }
+
+        public String getTag() {
+            return tag;
+        }
+
+        public Object getContent() {
+            return content;
+        }
+    }
+
 }
